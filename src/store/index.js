dd92e1b61edd837170fe75c34293ab7d8bb2acb3
rodrigobutta <rbutta@gmail.com>
2020-01-08 14:53:13 -0300

    clean

--- src/store/index.js
+++ src/store/index.js
@@ -5,22 +5,18 @@ import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 import rootReducer from './reducers';  
-// import rootSaga from './sagas';
-  
 
 const persistConfig = {
     key: 'root',
     storage: storage,
     whitelist: ['login', 'account', 'layout'],
-    stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.    
+    stateReconciler: autoMergeLevel2,   
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-// const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 const store = createStore(persistedReducer, {}, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 const persistor = persistStore(store);
