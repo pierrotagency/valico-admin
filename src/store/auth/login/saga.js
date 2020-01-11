b7e08bb6e11e9662cb852754db2da9fd4d5541aa
rodrigobutta <rbutta@gmail.com>
2020-01-11 09:18:57 -0300

    clean saga, prev updatere npmd

--- src/store/auth/login/saga.js
+++ src/store/auth/login/saga.js
@@ -1,15 +1,11 @@
-// import { takeEvery, fork, put, all, call } from 'redux-saga/effects';
-import { takeEvery, put, call } from 'redux-saga/effects';
+import { takeEvery, fork, put, all, call } from 'redux-saga/effects';
 
-// Login Redux States
 import { CHECK_LOGIN, GET_USER_INFO } from './actionTypes';
 import {  apiLoginError, loginUserSuccessful, getUserInfoOk, getUserInfoError } from './actions';
-
 import { apiGet, apiPost } from '../../../services/api';
 
-//If user is login then dispatch redux action's are directly from here.
-function* loginUser({ payload: { email, password, history } }) {
 
+function* loginUser({ payload: { email, password, history } }) {
     try {
         const response = yield call(apiPost, '/auth/login', {email: email, password: password});           
         yield put(loginUserSuccessful(response));
@@ -17,13 +13,9 @@ function* loginUser({ payload: { email, password, history } }) {
     } catch (error) {        
         yield put(apiLoginError(error));
     }
-
 }
 
-
-
 function* getUserInfo() {
-
     try {
         const response = yield call(apiGet, '/user/me', {});           
         yield put(getUserInfoOk(response));        
@@ -31,26 +23,30 @@ function* getUserInfo() {
         console.log(error)
         yield put(getUserInfoError(error));
     }
-
 }
 
 
-function* loginSaga() {
-    yield takeEvery(CHECK_LOGIN, loginUser);
-    yield takeEvery(GET_USER_INFO, getUserInfo);    
+export function* watchUserLogin() {
+    yield takeEvery(CHECK_LOGIN, loginUser)
 }
 
-// export function* watchUserLogin() {
-//     yield takeEvery(CHECK_LOGIN, loginUser)
+export function* watchGetUserInfo() {
+    yield takeEvery(GET_USER_INFO, getUserInfo)
+}
+
+export default function* loginSaga() {
+    yield all([
+        fork(watchUserLogin),
+        fork(watchGetUserInfo)
+    ]);
+}
+
+// export default function* loginSaga() {
+//     yield takeEvery(CHECK_LOGIN, loginUser);
+//     yield takeEvery(GET_USER_INFO, getUserInfo);    
 // }
 
-
-// export function* watchGetUserInfo() {
-//     yield takeEvery(GET_USER_INFO, getUserInfo)
+// export default function* loginSaga() {
+//     yield fork(takeEvery(CHECK_LOGIN, loginUser))
+//     yield fork(takeEvery(GET_USER_INFO, getUserInfo))   
 // }
-
-// function* loginSaga() {
-//     yield all([fork(watchUserLogin),fork(watchGetUserInfo)]);
-// }
-
-export default loginSaga;
