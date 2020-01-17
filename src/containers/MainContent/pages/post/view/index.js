a96641405f63e262ed3747bba9bfa1422d099f6f
rodrigobutta <rbutta@gmail.com>
2020-01-17 08:24:11 -0300

    View with meta and childs

--- src/containers/MainContent/pages/post/view/index.js
+++ src/containers/MainContent/pages/post/view/index.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
-import { Row, Col, Card, CardBody } from 'reactstrap';
-// import queryString from 'query-string'
+import React, { useEffect, useState } from 'react';
+import { Row, Col } from 'reactstrap';
 import { useParams } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-// import { useLocation, useHistory } from "react-router";
+import Toggle from 'react-toggle';
 
-import {log} from '../../../../../helpers/log'
+// import {log} from '../../../../../helpers/log'
 
 import { activateAuthLayout, getViewPost } from '../../../../../store/actions';
 import Settingmenu from '../../../Subpages/Settingmenu';
@@ -16,24 +15,10 @@ import Breadcrumb from '../_common/Breadcrumb';
 import CardWithLoading from '../../../../../components/CardWithLoading';
 import Select from '../../../../../components/Select';
 
-import { templates } from 'valico-sanmartin'
-
-// const templateOptions = [
-//     { value: 'Alaska', label: 'Alaska' },
-//     { value: 'Hawaii', label: 'Hawaii' },
-//     { value: 'California', label: 'California' },
-//     { value: 'Nevada', label: 'Nevada' },
-//     { value: 'Oregon', label: 'Oregon' },
-//     { value: 'Washington', label: 'Washington' }
-// ];
-
-
+import { templates, taxonomies, types } from 'valico-sanmartin'
 
 
 function PostEdit() {
-
-    // const location = useLocation();
-    // const history = useHistory();
 
     const post = useSelector(state => state.post.viewPost);    
     const loadingPost = useSelector(state => state.post.loadingViewPost);
@@ -41,27 +26,16 @@ function PostEdit() {
 
     let { id } = useParams();
 
-
     const templateOptions = Object.keys(templates).map((template) => ({ value: template, label: templates[template].name }))
+    const taxonomyOptions = Object.keys(taxonomies).map((taxonomy) => ({ value: taxonomy, label: taxonomies[taxonomy].name }))
+    const typeOptions = Object.keys(types).map((type) => ({ value: type, label: types[type].name }))
         
-    
-    // const changeUrlParam = (params) => {        
-
-    //     let qs = queryString.parse(location.search)
-    //         qs = {...qs, ...params}
-
-    //     const url = location.pathname + '?' + queryString.stringify(qs)    
-    //     history.push(url)        
-    // }
-
 
     useEffect(() => {      
         dispatch(activateAuthLayout())
-
         // window.addEventListener('popstate', (event) => {
         // alert("You message");
         // });
-        
     },[dispatch]);
 
     useEffect(() => {      
@@ -71,74 +45,164 @@ function PostEdit() {
     },[dispatch, id]);
 
 
+    const [live, setLive] = useState(true)
+    const handleAllowChildsToggle = (e) => setLive(e.target.checked) 
+
+
     const ParamsCard = () => {    
         
-        return (
+        return (           
             <>
-                <Card>
-                    <CardBody>
+                <h4 className="mt-0 header-title">Basic Properties</h4>
+                <p className="text-muted mb-4">Common to all posts</p>
 
-                        <h4 className="mt-0 header-title">Basic Properties</h4>
-                        <p className="text-muted mb-4">Common to all posts</p>
+                <form>
+                    <Row>
+                        <Col sm="6">
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <input id="name" name="name" type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="slug">Slug</label>
+                                <input id="slug" name="slug" type="text" className="form-control" />
+                            </div>
+                        </Col>
+                        <Col sm="6">                                 
+                            <div className="form-group">
+                                <label className="control-label">Type</label>
+                                <Select 
+                                    options={typeOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label">Taxonomy</label>
+                                <Select 
+                                    options={taxonomyOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label">Template</label>
+                                <Select 
+                                    options={templateOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="6">
+                        </Col>
+                        <Col sm="6">
+                        </Col>
+                    </Row>
 
-                        <form>
-                            <Row>
-                                <Col sm="6">
-                                    <div className="form-group">
-                                        <label htmlFor="name">Name</label>
-                                        <input id="name" name="name" type="text" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="slug">Slug</label>
-                                        <input id="slug" name="slug" type="text" className="form-control" />
-                                    </div>
-                                </Col>
-
-                                <Col sm="6">
-                                    <div className="form-group">
-                                        <label htmlFor="description">Description</label>
-                                        <textarea className="form-control" id="description" rows="5"></textarea>
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <Row>
-                                <Col sm="6">
-                                
-                                    <div className="form-group">
-                                        <label className="control-label">Template</label>
-
-                                        <Select 
-                                            options={templateOptions} 
-                                            placeholder={''}                                        
-                                        />
-
-
-                                    </div>
-                                </Col>
-
-                                <Col sm="6">
-                                    <div className="form-group">
-                                        <label>Image</label> <br />
-                                        <img src={img1} alt="product img" className="img-fluid rounded" style={{ maxWidth: "200px" }} />
-                                        <br />
-                                        <button type="button" className="btn btn-info mt-2 waves-effect waves-light">Change Image</button>
-                                    </div>
-                                </Col>
-                            </Row>
-
-                            <button type="submit" className="btn btn-success mr-1 waves-effect waves-light">Save Changes</button>
-                            <button type="submit" className="btn btn-secondary waves-effect">Cancel</button>
-                        </form>
-
-                    </CardBody>
-                </Card>
+                    <button type="submit" className="btn btn-success mr-1 waves-effect waves-light">Save Changes</button>
+                    <button type="submit" className="btn btn-secondary waves-effect">Cancel</button>
+                </form>
             </>
         )
 
     }
 
+    const ChildsCard = () => {    
+        
+        return (           
+            <>
+                <h4 className="mt-0 header-title">Childs</h4>
+                <p className="text-muted mb-4">sdsdaa</p>
+
+                <form>
+
+                    <Row>
+                        <Col sm="6">
+                            <label className="d-flex align-items-center mb-1">
+                                <Toggle defaultChecked={live} aria-label='Allow Childs' icons={false} onChange={handleAllowChildsToggle} />
+                                <span className="ml-2">Allow creating child pages for this page</span>
+                            </label>
+                            
+                        </Col>
+                        <Col sm="6">                                 
+                            <div className="form-group">
+                                <label className="control-label">Type</label>
+                                <Select 
+                                    options={typeOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label">Taxonomy</label>
+                                <Select 
+                                    options={taxonomyOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label className="control-label">Template</label>
+                                <Select 
+                                    options={templateOptions} 
+                                    placeholder={''}                                        
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <button type="submit" className="btn btn-success mr-1 waves-effect waves-light">Save Changes</button>
+                    <button type="submit" className="btn btn-secondary waves-effect">Cancel</button>
+                </form>
+            </>
+        )
+
+    }
+    
+    const MetaCard = () => {    
+        
+        return (           
+            <>
+                <h4 className="mt-0 header-title">Meta Data</h4>
+                <p className="text-muted mb-4">SEO and Social Sharing</p>
+
+                <form>
+                    <Row>
+                        <Col sm="6">
+                            <div className="form-group">
+                                <label htmlFor="metatitle">Meta Title</label>
+                                <input id="metatitle" name="productname" type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="metakeywords">Meta Keywords</label>
+                                <input id="metakeywords" name="manufacturername" type="text" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="metadescription">Meta Description</label>
+                                <textarea className="form-control" id="metadescription" rows="5"></textarea>
+                            </div>
+                        </Col>
+                        <Col sm="6">
+                            <div className="form-group">
+                                <label>Image</label> <br />
+                                <img src={img1} alt="product img" className="img-fluid rounded" style={{ maxWidth: "200px" }} />
+                                <br />
+                                <button type="button" className="btn btn-info mt-2 waves-effect waves-light">Change Image</button>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <button type="submit" className="btn btn-success mr-1 waves-effect waves-light">Save Changes</button>
+                    <button type="submit" className="btn btn-secondary waves-effect">Cancel</button>
+
+                </form>
+            </>
+        )
+
+    }
+    
+
     const ParamsCardWithLoading = CardWithLoading(ParamsCard);
+    const ChildsCardWithLoading = CardWithLoading(ChildsCard);
+    const MetaCardWithLoading = CardWithLoading(MetaCard);
 
     return (
         <>
@@ -165,41 +229,9 @@ function PostEdit() {
                         <Col>
 
                             <ParamsCardWithLoading isLoading={loadingPost} />   
+                            <ChildsCardWithLoading isLoading={loadingPost} />   
+                            <MetaCardWithLoading isLoading={loadingPost} />   
 
-                            <Card>
-                                <CardBody>
-
-                                    <h4 className="mt-0 header-title">Meta Data</h4>
-                                    <p className="text-muted mb-4">Fill all information below</p>
-
-                                    <form>
-                                        <Row>
-                                            <Col sm="6">
-                                                <div className="form-group">
-                                                    <label htmlFor="metatitle">Meta Title</label>
-                                                    <input id="metatitle" name="productname" type="text" className="form-control" />
-                                                </div>
-                                                <div className="form-group">
-                                                    <label htmlFor="metakeywords">Meta Keywords</label>
-                                                    <input id="metakeywords" name="manufacturername" type="text" className="form-control" />
-                                                </div>
-                                            </Col>
-
-                                            <Col sm="6">
-                                                <div className="form-group">
-                                                    <label htmlFor="metadescription">Meta Description</label>
-                                                    <textarea className="form-control" id="metadescription" rows="5"></textarea>
-                                                </div>
-                                            </Col>
-                                        </Row>
-
-                                        <button type="submit" className="btn btn-success mr-1 waves-effect waves-light">Save Changes</button>
-                                        <button type="submit" className="btn btn-secondary waves-effect">Cancel</button>
-
-                                    </form>
-
-                                </CardBody>
-                            </Card>
                         </Col>
                     </Row>
 
