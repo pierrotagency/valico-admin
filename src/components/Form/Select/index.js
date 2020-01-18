b1229fb54ff648d51c086625138c13e87b3d85a1
rodrigobutta <rbutta@gmail.com>
2020-01-17 21:03:39 -0300

    colors in Select

--- src/components/Form/Select/index.js
+++ src/components/Form/Select/index.js
@@ -6,6 +6,11 @@ const customStyles = {
         ...base,
         border: 0,
         boxShadow: 'none'
+    }),
+    option: (provided, state) => ({
+        ...provided,        
+        color: state.isSelected ? '#ffffff' : '#adb5bd',
+        // backgroundColor: '#3d4145'
     })
 };
 
@@ -23,13 +28,13 @@ export default ({name, onChange, ...props}) => {
                 ...theme,
                 colors: {
                 ...theme.colors,
-                    neutral0: '#3d4145',
+                    neutral0: '#3d4145', // background general
                     neutral5: '#0f0',
                     neutral10: '#00f',
-                    neutral20: '#adb5bd',
-                    neutral30: '#42464b',
-                    primary25: '#4090cb',
-                    primary: '#adb5bd',
+                    neutral20: '#adb5bd', // arrow and split in default mode
+                    neutral30: '#f00',
+                    primary25: '#4090cb', // list hover background
+                    primary: '#6c757d', // list selected item background
                     neutral50: '#6c757d', // placeholder
                     neutral60: '#fff', // arrow on focus
                     neutral80: '#fff', // Cursor of input on focus and selected text
