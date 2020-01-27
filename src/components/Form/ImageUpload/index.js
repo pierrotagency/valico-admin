7a47bbf97d751f7ea9382cf81690cb501211383f
rodrigobutta <rbutta@gmail.com>
2020-01-27 11:44:08 -0300

    cleanm

--- src/components/Form/ImageUpload/index.js
+++ src/components/Form/ImageUpload/index.js
@@ -101,7 +101,6 @@ const ImageUpload = ({label, name, value, className, isInvalid, isValid, message
     const addRequestBinds = (req) => {
             
         req.addEventListener("load", e => {
-            console.log('load')
 
             proxy.removeAllListeners(["abort"]);
             
@@ -215,7 +214,7 @@ const ImageUpload = ({label, name, value, className, isInvalid, isValid, message
                                 </div>
                             :
                                 <div aria-hidden="true" className="btn-cancel" onClick={cancelUpload} >                                    
-                                    <svg className="btn-cancel-svg" height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" className="css-6q0nyr-Svg"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
+                                    <svg className="btn-cancel-svg" height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
                                 </div>
                             }
                             
